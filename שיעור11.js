09b9dfd1c8984125f92fdb63aaded5ac3c33b551
dolevsa <147739924+dolevsa@users.noreply.github.com>
2023-12-28 19:18:14 +0200

    Add files via upload

---
+++ שיעור11.js
@@ -0,0 +1,23 @@
+let Amount = 0
+console.log(Amount);
+let Score1 = document.getElementById('Score')
+console.log(Score1);
+function add1() {
+    Amount = Amount + 1
+    Score.innerHTML = Amount
+}
+let Score10 = document.getElementById('Score')
+function add10() {
+    Amount = Amount + 10
+    Score.innerHTML = Amount
+}
+let S1core = document.getElementById('Score')
+function a1dd() {
+    Amount = Amount - 1
+    S1core.innerHTML = Amount
+}
+let S10core = document.getElementById('Score')
+function a10dd() {
+    Amount = Amount - 10
+    S10core.innerHTML = Amount
+}
